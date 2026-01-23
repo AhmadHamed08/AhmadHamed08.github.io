5f085fb625a1f9165b023e7769e3979513dfe7e5
AhmadHamed08 <ahmad.omar.hamed@gmail.com>
2026-01-23 13:14:45 +0100

    react fun

---
+++ react/main.js
@@ -0,0 +1,50 @@
+function App(){
+    return(
+        <div className ="app">
+            <Header></Header>
+            <h2>REACT</h2>
+
+            <Students></Students>
+
+            <Footer></Footer>
+
+        </div>
+
+    )
+}
+
+function Header(){
+    return(
+        <header>
+            <nav>
+                <a href="/">HOME</a>
+                <a href="#contact">CONTACT</a>
+            </nav>
+        </header>
+    )
+}
+
+function Footer(){
+    return(
+        <footer>
+            <h3>This is the end of the page</h3>
+        </footer>
+    )
+}
+
+function Students(){
+    const stud = ["Dennis", "Mahmoud", "Maheen"];
+
+    return(
+        <div className="students">
+            {stud.map(s=>(
+                <h4>{s}</h4>
+            ))}
+        </div>
+    )
+}
+
+
+
+const el = document.querySelector("#root");
+ReactDOM.createRoot(el).render(App())
